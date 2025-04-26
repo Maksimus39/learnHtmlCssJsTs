1b6ec1c6b510214db7d5e5e242c26a69ccf8f51d
Max <maksim.minakov.83@mail.ru>
2025-04-26 21:18:07 +0300

    Lesson 03 - Занятие по JS Методы массивов, slice, splice, toSpliced, reduce

---
+++ IT-incubator-JS/sprint-1/lesson-3/main.js
@@ -0,0 +1,19 @@
+// const arr = [1, 2, 3, 4, 5]
+// const arr2 = new Array(1, 2, 3, 4, 5)
+//
+//
+// // arr.push = function () {
+// //     console.log("Я не тот push!!!")
+// // }
+//
+// console.log(arr)
+// console.log(arr2)
+//
+// console.log(arr===arr2)
+//
+// console.log(Array.prototype)
+// console.log(arr2.__proto__)
+
+
+// slice
+
