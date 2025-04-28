dcd3af46c182bdc3918597a5ba4dbf4ee53a97a6
Max <maksim.minakov.83@mail.ru>
2025-04-28 11:23:04 +0300

    Lesson 03 - Занятие по JS Методы массивов, slice, splice, toSpliced, reduce - продолжение

--- IT-incubator-JS/sprint-1/lesson-3/main.js
+++ IT-incubator-JS/sprint-1/lesson-3/main.js
@@ -1,4 +1,4 @@
-// const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5, 6]
 // const arr2 = new Array(1, 2, 3, 4, 5)
 //
 //
@@ -15,5 +15,36 @@
 // console.log(arr2.__proto__)
 
 
-// slice
+// ---------------------------------------------------------------------
+// Метод  map
+
+Array.prototype.myMap = function forMap(callback) {
+    const result = []
+    for (let index = 0; index < this.length; index++) {
+        result[index] = callback(this[index]);
+    }
+    return result
+}
+
+console.log(arr.myMap(n => n ** 5))
+const bigNumbers = arr.myMap(n => n ** 5)
+console.log(bigNumbers)
+// ------------------------------------------------------------------------
+
+// Метод filter
+Array.prototype.myFilter = function (callback) {
+    const result = []
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i]) === true) {
+            result.push(this[i])
+        }
+    }
+    return result
+}
+
+console.log("bigNumbers:", bigNumbers.myFilter(n => n > 1000))
+
+
+
 
