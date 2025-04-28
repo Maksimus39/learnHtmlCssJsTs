7b5d074d1c6f6b8c500cda7585cb333be674e2df
Max <maksim.minakov.83@mail.ru>
2025-04-28 12:11:01 +0300

    Операторы prefix и postfix

--- JS с нуля жо профи/Практическое занятие 2_1 по переменным/main.js
+++ JS с нуля жо профи/Практическое занятие 2_1 по переменным/main.js
@@ -9,3 +9,16 @@ console.log(typeof str2)
 console.log(typeof val1)
 console.log(typeof val2)
 console.log(typeof myNum)
+
+
+// Операторы prefix и postfix
+
+let nr = 2;
+console.log(nr++);
+console.log(nr);
+
+
+let nr1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+console.log(nr1++ + ++nr2 * nr3++); // 4 + ( 6 * 2) = 16 good)))
