880cad5f130e322795f3e8142d0b0d6402264f1c
Max <maksim.minakov.83@mail.ru>
2025-04-28 12:04:48 +0300

    Практическое занятие 2.2

---
+++ JS с нуля жо профи/Практическое занятие 2_1 по переменным/Операторы/main.js
@@ -0,0 +1,20 @@
+// Сложение
+
+const pr1 = 12
+const pr2 = 14
+
+const result = pr1 + pr2
+console.log(result)
+
+let str1 = "Hello ";
+let str2 = "addition";
+let result2 = str1 + str2;
+console.log(result2)
+
+// Практическое занятие 2.2
+const lastName = "Maksim"
+const myAge = 42
+const isCodingJavaScript = true
+
+console.log(`Hello, my name is ${lastName}, I am ${myAge} years old and I can code JavaScript: ${isCodingJavaScript}`)
+
